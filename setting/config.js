d2f5746004d326f639f80a2e86d51d723d81fa6b
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-04-03 17:14:45 +0300

    Add setting/config.js

---
+++ setting/config.js
@@ -0,0 +1,24 @@
+global.botName     = '𝗧𝗼𝘅𝗶𝗰𝗕𝗼𝘁'
+global.botAuthor   = '𝘅𝗵𝗰𝗹𝗶𝗻𝘁𝗼𝗻'
+global.tittle   = '𝗧𝗼𝘅𝗶𝗰𝘅𝗕𝗼𝘁𝘇'
+global.botVersion  = '1.0.0'
+
+global.Jid = `120363420097356422@newsletter`
+global.thumb = 'https://i.top4top.io/p_3704rafds1.jpg'
+global.thumbLocal = './image/legend.jpg'
+
+global.connect = true
+global.publicX = true
+global.owner = [
+  '254735342808',
+]
+global.timezone = 'Asia/Karachi'
+global.prefix = '.'
+
+global.packName   = '𝗧𝗼𝘅𝗶𝗰𝗕𝗼𝘁'
+global.packAuthor = '𝘅𝗵𝗰𝗹𝗶𝗻𝘁𝗼𝗻'
+global.sessionFolder = './session'
+global.ownPath    = './Access/Own.json'
+global.premPath   = './Access/Prem.json'
+global.idgrpPath  = './Access/idgrup.json'
+global.dbPath = './Database/db.json'
